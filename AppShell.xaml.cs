a26b40eccdba055e8389427a120cea88657d0600
Oana-Maria Asandului <oanamaria1415@yahoo.com>
2024-11-21 20:18:30 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AsanduluiOanaMariaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
